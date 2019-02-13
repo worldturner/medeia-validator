3b5622da4150eba024255c335c1f682da90ec4d6
Erwin Bolwidt <ebolwidt@worldturner.com>
2019-02-14 09:49:20 +1100

    Added ipv4 and ipv6 formats

--- medeia-validator-core/src/main/kotlin/com/worldturner/medeia/schema/validation/FormatValidator.kt
+++ medeia-validator-core/src/main/kotlin/com/worldturner/medeia/schema/validation/FormatValidator.kt
@@ -31,77 +31,110 @@ class FormatValidator(
             if (formatRegexes[format]?.matchEntire(string) != null)
                 OkValidationResult
             else
-                FailedValidationResult(
-                    location = location,
-                    rule = "format",
-                    message = "Data does not match format '$format'"
-                )
+                failedValidation(string, location)
         } else when (format) {
             "regex" -> try {
                 Regex(string).let { OkValidationResult }
             } catch (e: PatternSyntaxException) {
-                FailedValidationResult(
-                    location = location,
-                    rule = "format",
-                    message = "Invalid regex syntax"
-                )
+                failedValidation(string, location, e)
             }
-            "uri-reference", "uri", "iri-reference", "iri" -> try {
-                URI.create(string).let { OkValidationResult }
-            } catch (e: IllegalArgumentException) {
-                FailedValidationResult(
-                    location = location,
-                    rule = "format",
-                    message = "Invalid regex syntax"
-                )
-            }
+            "uri-reference", "uri", "iri-reference", "iri" ->
+                try {
+                    URI.create(string).let { OkValidationResult }
+                } catch (e: IllegalArgumentException) {
+                    failedValidation(string, location, e)
+                }
             "json-pointer" ->
                 try {
                     JsonPointer(string).let { OkValidationResult }
                 } catch (e: IllegalArgumentException) {
-                    FailedValidationResult(
-                        location = location,
-                        rule = "format",
-                        message = "Invalid json-pointer syntax: ${e.message}"
-                    )
+                    failedValidation(string, location, e)
                 }
             "date" ->
                 try {
                     LocalDate.parse(string).let { OkValidationResult }
                 } catch (e: DateTimeParseException) {
-                    FailedValidationResult(
-                        location = location,
-                        rule = "format",
-                        message = "Invalid time: ${e.message}"
-                    )
+                    failedValidation(string, location, e)
                 }
             "time" ->
                 try {
                     OffsetTime.parse(string).let { OkValidationResult }
                 } catch (e: DateTimeParseException) {
-                    FailedValidationResult(
-                        location = location,
-                        rule = "format",
-                        message = "Invalid time: ${e.message}"
-                    )
+                    failedValidation(string, location, e)
                 }
             "date-time" ->
                 try {
                     OffsetDateTime.parse(string).let { OkValidationResult }
                 } catch (e: DateTimeParseException) {
-                    FailedValidationResult(
-                        location = location,
-                        rule = "format",
-                        message = "Invalid date-time: ${e.message}"
-                    )
+                    failedValidation(string, location, e)
                 }
-
+            "ipv4" ->
+                if (string.isIpv4())
+                    OkValidationResult
+                else
+                    failedValidation(string, location)
+            "ipv6" ->
+                if (string.isIpv6())
+                    OkValidationResult
+                else
+                    failedValidation(string, location)
             else -> OkValidationResult
         }
     }
+
+    fun failedValidation(string: String, location: JsonTokenLocation) =
+        FailedValidationResult(
+            location = location,
+            rule = "format",
+            message = "Invalid $format '$string'"
+        )
+
+    fun failedValidation(string: String, location: JsonTokenLocation, e: Exception) =
+        FailedValidationResult(
+            location = location,
+            rule = "format",
+            message = "Invalid $format '$string': ${e.message}"
+        )
 
     companion object {
         fun create(format: String?): FormatValidator? =
             format?.let { FormatValidator(format) }
     }
+}
+
+private fun String.isIpv4(): Boolean {
+    val decbytes = this.split('.')
+    if (decbytes.size != 4) return false
+    if (!decbytes.all { it.isDecbyte() }) return false
+    if (!decbytes.map { it.toInt() }.all { it in 0..255 }) return false
+    return true
+}
+
+private fun String.isIpv6(): Boolean {
+    val xs = this.split(':')
+    if (xs.size > 8) return false
+    if (!xs.all { it.isIpv6Hex() }) return false
+    if (!xs.map { if (it.isEmpty()) 0 else it.toInt(16) }.all { it in 0..65535 }) return false
+    return true
+}
+
+private fun String.isDecbyte(): Boolean {
+    val l = this.length
+    if (l == 0) return false
+    for (i in 0 until l) {
+        if (this[i] !in '0'..'9') return false
+    }
+    return true
+}
+
+private fun String.isIpv6Hex(): Boolean {
+    val l = this.length
+    for (i in 0 until l) {
+        when (this[i]) {
+            in '0'..'9', in 'a'..'f', in 'A'..'F' -> {
+            }
+            else -> return false
+        }
+    }
+    return true
 }
