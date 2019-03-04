48470238254fb45b7fd18593b8fb1f6446fc310e
Erwin Bolwidt <ebolwidt@worldturner.com>
2019-03-04 14:50:41 +1100

    Added java example

---
+++ medeia-validator-java-examples/src/main/java/com/worldturner/medeia/examples/java/readobject1/Address.java
@@ -0,0 +1,49 @@
+package com.worldturner.medeia.examples.java.readobject1;
+
+public class Address {
+    private String street1;
+    private String street2;
+    private String city;
+    private String postalCode;
+    private String country;
+
+    public String getStreet1() {
+        return street1;
+    }
+
+    public void setStreet1(String street1) {
+        this.street1 = street1;
+    }
+
+    public String getStreet2() {
+        return street2;
+    }
+
+    public void setStreet2(String street2) {
+        this.street2 = street2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
---
+++ medeia-validator-java-examples/src/main/java/com/worldturner/medeia/examples/java/readobject1/Person.java
@@ -0,0 +1,31 @@
+package com.worldturner.medeia.examples.java.readobject1;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private Address address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
