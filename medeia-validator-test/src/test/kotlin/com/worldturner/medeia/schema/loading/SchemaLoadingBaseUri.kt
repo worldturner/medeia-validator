e783b78ddfbb571219e58d9721707edd704abdf1
Erwin Bolwidt <ebolwidt@worldturner.com>
2019-03-19 08:34:51 +1100

    Make schema accessible by provided baseUri too even if it has its own $id/id
    Add features that allow a highly efficient component (like a web resource) to do validation and forwarding of data

---
+++ medeia-validator-test/src/test/kotlin/com/worldturner/medeia/schema/loading/SchemaLoadingBaseUri.kt
@@ -0,0 +1,49 @@
+package com.worldturner.medeia.schema.loading
+
+import com.worldturner.medeia.api.JsonSchemaVersion
+import com.worldturner.medeia.api.StringSchemaSource
+import com.worldturner.medeia.api.jackson.MedeiaJacksonApi
+import com.worldturner.medeia.schema.validation.SchemaValidator
+import org.hamcrest.BaseMatcher
+import org.hamcrest.Description
+import org.hamcrest.Matchers.aMapWithSize
+import org.hamcrest.Matchers.hasKey
+import org.junit.Assert.assertThat
+import org.junit.Test
+import java.net.URI
+
+private val TEST_SCHEMA = """
+{
+    "${"$"}id": "http://foo.bar/",
+    "minItems": 4
+}
+""".trimIndent()
+
+class SchemaLoadingBaseUri {
+    val medeia = MedeiaJacksonApi()
+
+    @Test
+    fun `Schema loaded with baseUri which has a $id should be accessible by both`() {
+        val schemaMap = mutableMapOf<URI, SchemaValidator>()
+        val source =
+            StringSchemaSource(
+                TEST_SCHEMA,
+                version = JsonSchemaVersion.DRAFT07,
+                baseUri = URI.create("schemas/foobar.json")
+            )
+        medeia.loadSchemas(listOf(source), schemaMap)
+        assertThat(schemaMap, aMapWithSize(object : BaseMatcher<Int>() {
+            override fun matches(actual: Any?): Boolean =
+                actual as Int >= 3
+
+            override fun describeTo(description: Description) {
+                description
+                    .appendText("greater than or equal to ")
+                    .appendValue(3)
+            }
+        }))
+        assertThat(schemaMap, hasKey(URI.create("http://foo.bar/")))
+        assertThat(schemaMap, hasKey(URI.create("http://foo.bar/#")))
+        assertThat(schemaMap, hasKey(URI.create("schemas/foobar.json")))
+    }
+}
--- medeia-validator-test/src/test/kotlin/com/worldturner/medeia/schema/loading/SchemaLoadingErrorReporting.kt
+++ medeia-validator-test/src/test/kotlin/com/worldturner/medeia/schema/loading/SchemaLoadingErrorReporting.kt
@@ -1,33 +1,45 @@
 package com.worldturner.medeia.schema.loading
 
 import com.worldturner.medeia.api.JsonSchemaVersion
-import com.worldturner.medeia.api.ReaderSchemaSource
+import com.worldturner.medeia.api.StringSchemaSource
+import com.worldturner.medeia.api.TokenLocationException
 import com.worldturner.medeia.api.ValidationFailedException
 import com.worldturner.medeia.api.ValidationOptions
 import com.worldturner.medeia.api.jackson.MedeiaJacksonApi
 import org.hamcrest.Matchers.stringContainsInOrder
 import org.junit.Assert.assertThat
 import org.junit.Test
-import java.io.StringReader
 
-class SchemaLoadingErrorReporting {
-
-    @Test(expected = ValidationFailedException::class)
-    fun testLoadStrangeSchema() {
-        val medeia = MedeiaJacksonApi()
-        val r = StringReader(
-            """
+private val TEST_SCHEMA = """
 {
     "minItems": "foobar"
 }
-            """.trimIndent()
-        )
-        val source = ReaderSchemaSource(r, version = JsonSchemaVersion.DRAFT07, name = "string")
+""".trimIndent()
+
+class SchemaLoadingErrorReporting {
+    val medeia = MedeiaJacksonApi()
+
+    @Test(expected = ValidationFailedException::class)
+    fun `Validation error in schema should include line, column and filename`() {
+        val source = StringSchemaSource(TEST_SCHEMA, version = JsonSchemaVersion.DRAFT07, name = "input-string")
         try {
             medeia.loadSchemas(listOf(source), options = ValidationOptions(validateSchema = true))
         } catch (e: ValidationFailedException) {
             assertThat(e.failures.first().location, stringContainsInOrder("at 2:25 "))
-            println(e)
+            assertThat(e.failures.first().location, stringContainsInOrder("in input-string"))
+            throw e
+        }
+    }
+
+    @Test(expected = TokenLocationException::class)
+    fun `Object mapping error in schema should include line, column and filename`() {
+        val source =
+            StringSchemaSource(TEST_SCHEMA, version = JsonSchemaVersion.DRAFT07, name = "input-string")
+        try {
+            medeia.loadSchemas(listOf(source), options = ValidationOptions(validateSchema = false))
+        } catch (e: TokenLocationException) {
+            assertThat(e.location, stringContainsInOrder("at 2:25 "))
+            assertThat(e.location, stringContainsInOrder("in input-string"))
             throw e
         }
     }
